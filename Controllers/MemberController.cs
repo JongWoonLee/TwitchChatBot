2f9a4978f06dea44e3c71af849272958ad3f53ac
JongWoonLee <jongwoon96@toothlife.co.kr>
2021-07-27 18:12:39 +0900

    누락된 로그인 화면 추가.

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -29,7 +29,7 @@ namespace TwitchChatBot.Controllers
         /// Twitch Login
         /// </summary>
         /// <param name="Code">Token을 가져오기 위한 AccessCode</param>
-        /// <returns></returns>
+        /// <returns>IActionResult</returns>
         [HttpGet, Route("/member/index")]
         public IActionResult Index(string Code)
         {
@@ -40,7 +40,7 @@ namespace TwitchChatBot.Controllers
                 {
                     string url = MemberService.GetRedirectURL();
                     return Redirect(url);
-                } // end if
+                }
                 TwitchToken TwitchToken = MemberService.GettingTokens(Code); // code 가 있으면 TwitchToken을 얻어오고
                 User User = MemberService.ValidatingRequests(TwitchToken.AccessToken); // TwitchToken 을 이용해서 User를 얻어오고
                 int InsertResult = MemberService.FindStreamer(User.UserId).StreamerIsValid() ?
@@ -52,19 +52,25 @@ namespace TwitchChatBot.Controllers
                     Response.Cookies.Append("AccessToken", TwitchToken.AccessToken);
                     Response.Cookies.Append("UserId", Convert.ToString(User.UserId));
                     Response.Cookies.Append("ChannelName", User.Login);
-                } // end if
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-            } // end try
+            }
             return RedirectToAction("Index", "Home");
-        } // end Index
+        }
+
+        [HttpGet, Route("/member/login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         /// <summary>
         /// StreamerDetail 정보를 설정하는 페이지
         /// </summary>
-        /// <returns></returns>
+        /// <returns>IActionResult</returns>
         [HttpGet, Route("/member/details")]
         public IActionResult Details()
         {
@@ -72,15 +78,16 @@ namespace TwitchChatBot.Controllers
             long StreamerId = Convert.ToInt64(UserId);
             ViewData["StreamerDetail"] = MemberService.FindStreamerDetail(StreamerId); // StreamerId 를 가지고 StreamerDetail정보를 읽어온다.
             return View();
-        } // end Details
+        }
 
         /// <summary>
         /// StreamerDetail 정보를 수정
         /// </summary>
-        /// <returns></returns>
+        /// <returns>string 실행결과</returns>
         [HttpPost, Route("/member/details")]
         public string DetailsPost()
         {
+            string Result = "";
             try
             {
                 /// 입력받은 StreamerDetail 정보를 읽어오고 Update
@@ -97,20 +104,20 @@ namespace TwitchChatBot.Controllers
                     var ForbiddenWordTimeout = Convert.ToInt32(JObject["forbiddenWordTimeout"]);
                     StreamerDetail StreamerDetail = new StreamerDetail(StreamerId, DonationLink, GreetingMessage, ForbiddenWordLimit ? 1 : 0, ForbiddenWordTimeout);
                     MemberService.UpdateStreamerDetail(StreamerDetail); // StreamerDetail 정보를 Update
-                } // end using
-                return "success";
+                }
+                Result = "success";
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-                return "failed";
-            } // end try
-        } // end DetailsPost
+            }
+            return Result;
+        }
 
         /// <summary>
         /// 회원의 봇 사용 여부를 설정하는 페이지
         /// </summary>
-        /// <returns></returns>
+        /// <returns>IActionResult</returns>
         [HttpGet, Route("/member/startbot")]
         public IActionResult StartBot()
         {
@@ -121,14 +128,14 @@ namespace TwitchChatBot.Controllers
                 int Result = MemberService.FindBotInUseByUserId(UserId); // 유저가 봇을 사용하는지 여부를 읽어온다.
                 ViewData["Result"] = Result;
                 return View();
-            } // end if
+            } 
             return RedirectToAction("Index", "Home");
-        } // end StartBot
+        }
 
         /// <summary>
         /// 봇 사용 여부를 받아 봇을 실행/폐기
         /// </summary>
-        /// <returns></returns>
+        /// <returns>string 명령 success/fail</returns>
         [HttpPost, Route("/member/startbot")]
         public string StartBotPost()
         {
@@ -141,7 +148,7 @@ namespace TwitchChatBot.Controllers
                     var Body = Reader.ReadToEndAsync();
                     JObject JObject = JObject.Parse(Body.Result);
                     BotInUse = (int)JObject["BotInUse"]; // Request에 들어있는 BotInUse
-                } // end using
+                }
                 string UserId = Request.Cookies["UserId"];
                 long StreamerId = Convert.ToInt64(UserId);
                 string ChannelName = Request.Cookies["ChannelName"];
@@ -159,7 +166,7 @@ namespace TwitchChatBot.Controllers
                 {
                     MemberService.UpdateStreamerDetailBotInUse(StreamerId, BotInUse); // 봇 사용 여부를 Update
                     Result = ThreadExecutorService.DisposeBot(StreamerId); // 봇을 폐기
-                } // end if
+                }
             }
             catch(Exception e)
             {
@@ -167,12 +174,12 @@ namespace TwitchChatBot.Controllers
             }
             
             return Result;
-        } // end StartBotPost
+        }
 
         /// <summary>
         /// Logout 하고 브라우저 정보 초기화
         /// </summary>
-        /// <returns></returns>
+        /// <returns>IActionResult</returns>
         [HttpGet, Route("/member/logout")]
         public IActionResult Logout()
         {
@@ -183,23 +190,34 @@ namespace TwitchChatBot.Controllers
                 Response.Cookies.Delete("ChannelName");
             }
             return RedirectToAction("Index", "Home");
-        } // end Logout()
+        }
 
         /// <summary>
         /// 금지어 설정 페이지
         /// </summary>
-        /// <returns></returns>
+        /// <returns>IActionResult</returns>
         [HttpGet, Route("/member/words")]
         public IActionResult Words()
         {
-            string UserId = Request.Cookies["UserId"];
-            long StreamerId = Convert.ToInt64(UserId);
-            var ForbiddenWordList = MemberService.FindForbiddenWordList(StreamerId); // 유저의 금지어 정보를 읽어온다
+            try
+            {
+                string UserId = Request.Cookies["UserId"];
+                long StreamerId = Convert.ToInt64(UserId);
+                var ForbiddenWordList = MemberService.FindForbiddenWordList(StreamerId); // 유저의 금지어 정보를 읽어온다
 
-            ViewData["ForbiddenWordList"] = ForbiddenWordList;
+                ViewData["ForbiddenWordList"] = ForbiddenWordList;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
             return View();
-        } // end Words
+        }
 
+        /// <summary>
+        /// 금지어 삽입,수정,삭제 설정 POST 메서드
+        /// </summary>
+        /// <returns>string 완료 작업</returns>
         [HttpPost, Route("/member/words")]
         public string WordsPost()
         {
@@ -218,7 +236,7 @@ namespace TwitchChatBot.Controllers
                     ForbiddenWord = ((string)JObject["ForbiddenWord"]).Trim(); // 금지어
                     Todo = (string)JObject["Todo"]; // 작업의 이름
                     PrevWord = (string)JObject["PrevWord"]; // 이전 입력 내용(Update 시에만 필요)
-                } // end using
+                }
 
                 string UserId = Request.Cookies["UserId"];
                 long StreamerId = Convert.ToInt64(UserId);
@@ -229,25 +247,25 @@ namespace TwitchChatBot.Controllers
                     switch (Todo)
                     {
                         case "Insert":
-                            Result = MemberService.InsertForbiddenWord(StreamerId, ForbiddenWord);
-                            ThreadExecutorService.ManagedBot[StreamerId].RenewForbiddenWordList();
-                            return "Insert";
+                            Result = MemberService.InsertForbiddenWord(StreamerId, ForbiddenWord); // 금지어 Insert 후
+                            ThreadExecutorService.ManagedBot[StreamerId].RenewForbiddenWordList(); // 해당 유저의 봇 금지어 정보 갱신
+                            break;
                         case "Update":
-                            Result = MemberService.UpdateForbiddenWord(StreamerId, ForbiddenWord, PrevWord);
-                            ThreadExecutorService.ManagedBot[StreamerId].RenewForbiddenWordList();
-                            return "Update";
+                            Result = MemberService.UpdateForbiddenWord(StreamerId, ForbiddenWord, PrevWord); // 금지어 Update 후
+                            ThreadExecutorService.ManagedBot[StreamerId].RenewForbiddenWordList();           // 해당 유저의 봇 금지어 정보 갱신
+                            break;
                         case "Delete":
-                            Result = MemberService.DeleteForbiddenWord(StreamerId, ForbiddenWord);
-                            ThreadExecutorService.ManagedBot[StreamerId].RenewForbiddenWordList();
-                            return "Delete";
-                    } // end switch
+                            Result = MemberService.DeleteForbiddenWord(StreamerId, ForbiddenWord); // 금지어 Delete 후
+                            ThreadExecutorService.ManagedBot[StreamerId].RenewForbiddenWordList(); // 해당 유저의 봇 금지어 정보 갱신
+                            break;
+                    } 
                 }
             }
             catch(Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-            return null;
-        } // end WordsPost
+            return Todo;
+        }
     } // end class
 } // end namespace
